ec957725f8575d1aec61ebc808771d19de855d6e
Dragonspine <ghoul300@naver.com>
2022-09-30 22:22:43 +0900

    feat: type converter 소개

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,42 @@
+package hello.typeconverter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * packageName    : hello.typeconverter.controller
+ * fileName       : HelloController
+ * author         : kanghyun Kim
+ * date           : 2022/09/30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/09/30        kanghyun Kim      최초 생성
+ */
+@Slf4j
+@RestController
+public class HelloController {
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        // request.getParameter 는 무조건 문자로 처리된다.
+        String data = request.getParameter("data"); // 문자 타입 조회
+        Integer intValue = Integer.valueOf(data);
+        log.info("intVal = {}", intValue);
+        return "ok";
+    }
+
+    /**
+     * RequestParam, ModelAttribute, PathVariable 스프링이 자동으로 타입변환 해줌
+     */
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("data = {}", data);
+        return "ok";
+    }
+
+}
