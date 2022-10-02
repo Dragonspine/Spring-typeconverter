1ab6e49e6517f4cbc26eeb925c2e079134b7c656
Dragonspine <ghoul300@naver.com>
2022-10-02 13:28:19 +0900

    feat: type converter - 정리

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -25,6 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     /**
      * WebConfig - 컨버터, 포멧터 등록
+     * 컨버전 서비스는 @RequestParam , @ModelAttribute , @PathVariable 뷰 템플릿 등에서 사용할 수 있다.
+     * API통신시 JSON를 객체로 변환할 때는 못쓰고, Jackson 같은 라이브러리의 설정을 통해 포멧을 변경해야 한다.
      */
     @Override
     public void addFormatters(FormatterRegistry registry) {
