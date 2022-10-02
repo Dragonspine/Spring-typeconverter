922511bf5a9722de32a71b4f506da1003b9a5acb
Dragonspine <ghoul300@naver.com>
2022-10-02 12:32:03 +0900

    feat: type converter - thymeleaf 에 converter 적용하기

---
+++ src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -0,0 +1,61 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import lombok.Data;
+import org.springframework.boot.Banner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * packageName    : hello.typeconverter.controller
+ * fileName       : ConverterController
+ * author         : kanghyun Kim
+ * date           : 2022/10/02
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/10/02        kanghyun Kim      최초 생성
+ */
+@Controller
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter-view";
+    }
+
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+        model.addAttribute("form", form);
+        return "converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "converter-view";
+
+    }
+
+
+    @Data
+    static class Form {
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
+    }
+
+
+}
